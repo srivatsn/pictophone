34797f80b2156677c867984e6f75995ff0728059
Srivatsn Narayanan <srivatsn@users.noreply.github.com>
2024-09-08 05:09:18 +0000

    Host in the network

--- start.js
+++ start.js
@@ -5,6 +5,7 @@ const startServer = async () => {
     const server = await createServer({
         server: {
             port: process.env.PORT || 3000,
+            host: '0.0.0.0', // Expose the server to the network
         },
     });
     await server.listen();
